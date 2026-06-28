ea288ff4da0a203cb84a7520ed64346838eb2a37
Zhiqiang Du <haxck@qq.com>
2026-06-28 16:59:03 +0800

    feat: Update GitHub Actions workflow and scripts for improved deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.VITE_BASE || "/",
+export default defineConfig(({ mode }) => ({
+  base: mode === 'github' ? '/about/' : '/',
   plugins: [vue(),tailwindcss()],
-});
+}));
