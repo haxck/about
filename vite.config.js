3d0d7a1af5771359b8450d864332510f3bb4193f
Zhiqiang Du <haxck@qq.com>
2026-06-28 16:35:54 +0800

    feat: Add GitHub Actions workflow for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from "@tailwindcss/vite";
- 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: process.env.VITE_BASE || "/",
   plugins: [vue(),tailwindcss()],
 });
